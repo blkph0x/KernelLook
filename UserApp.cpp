e6bb459b85cc38a43fa0379e32eab21ba91c4083
BlkPh0x <blkph0x@gmail.com>
2023-05-21 14:14:09 +1000

    Update UserApp.cpp
    updates to fix input-output buffers

--- UserApp.cpp
+++ UserApp.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
-#include <vector>
 #include <Windows.h>
+#include <stdio.h>
 
 #define IOCTL_GET_PROCESS_IDS CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define IOCTL_GET_MODULES CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+#define MAX_PROCESS_INFO_COUNT 1000 // Maximum number of process info entries
+#define MAX_MODULE_INFO_COUNT 1000  // Maximum number of module info entries
 
 typedef struct _PROCESS_INFO {
     HANDLE ProcessId;
@@ -18,44 +20,112 @@ typedef struct _MODULE_INFO {
 
 int main()
 {
-    HANDLE hDevice = CreateFile(L"\\\\.\\ModuleList", GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
-
-    if (hDevice == INVALID_HANDLE_VALUE) {
-        std::cout << "Failed to open device. Error: " << GetLastError() << std::endl;
-        return 1;
-    }
-
+    HANDLE hDevice = INVALID_HANDLE_VALUE;
     DWORD bytesReturned = 0;
-    std::vector<PROCESS_INFO> processInfo(256);
-    BOOL result = DeviceIoControl(hDevice, IOCTL_GET_PROCESS_IDS, nullptr, 0, processInfo.data(), processInfo.size() * sizeof(PROCESS_INFO), &bytesReturned, nullptr);
 
-    if (!result) {
-        std::cout << "Failed to get process ids. Error: " << GetLastError() << std::endl;
+    // Open a handle to the driver device
+    hDevice = CreateFile(L"\\\\.\\ModuleList", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hDevice == INVALID_HANDLE_VALUE)
+    {
+        printf("Failed to open device. Error: %d\n", GetLastError());
+        return 1;
+    }
+
+    // Get the required buffer size for process info
+    ULONG processInfoSize = 0;
+    if (!DeviceIoControl(hDevice, IOCTL_GET_PROCESS_IDS, NULL, 0, NULL, 0, &bytesReturned, NULL))
+    {
+        if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
+        {
+            printf("Failed to get process info size. Error: %d\n", GetLastError());
+            CloseHandle(hDevice);
+            return 1;
+        }
+
+        processInfoSize = bytesReturned;
+    }
+
+    // Allocate memory for process info
+    PPROCESS_INFO pProcessInfo = (PPROCESS_INFO)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, processInfoSize);
+    if (pProcessInfo == NULL)
+    {
+        printf("Failed to allocate memory for process info.\n");
         CloseHandle(hDevice);
         return 1;
     }
 
-    for (unsigned int i = 0; i < bytesReturned / sizeof(PROCESS_INFO); i++) {
-        std::wcout << L"Process ID: " << processInfo[i].ProcessId << L", Name: " << processInfo[i].Name << std::endl;
-    }
-
-    std::cout << "Enter process id to get modules: ";
-    ULONG processId;
-    std::cin >> processId;
-
-    std::vector<MODULE_INFO> moduleInfo(256);
-    result = DeviceIoControl(hDevice, IOCTL_GET_MODULES, &processId, sizeof(processId), moduleInfo.data(), moduleInfo.size() * sizeof(MODULE_INFO), &bytesReturned, nullptr);
-
-    if (!result) {
-        std::cout << "Failed to get modules. Error: " << GetLastError() << std::endl;
+    // Get process info
+    if (!DeviceIoControl(hDevice, IOCTL_GET_PROCESS_IDS, NULL, 0, pProcessInfo, processInfoSize, &bytesReturned, NULL))
+    {
+        printf("Failed to get process info. Error: %d\n", GetLastError());
+        HeapFree(GetProcessHeap(), 0, pProcessInfo);
         CloseHandle(hDevice);
         return 1;
     }
 
-    for (unsigned int i = 0; i < bytesReturned / sizeof(MODULE_INFO); i++) {
-        std::wcout << L"Module Base: " << moduleInfo[i].Base << L", Size: " << moduleInfo[i].Size << L", Name: " << moduleInfo[i].Name << std::endl;
+    // Print process info
+    printf("Process Info:\n");
+    for (DWORD i = 0; i < bytesReturned / sizeof(PROCESS_INFO); i++)
+    {
+        printf("Process ID: %lu, Name: %ls\n", pProcessInfo[i].ProcessId, pProcessInfo[i].Name);
+    }
+    printf("\n");
+
+    // Prompt user to enter a process ID for module info
+    printf("Enter a process ID to get module info: ");
+    DWORD processId = 0;
+    scanf_s("%lu", &processId);
+
+    // Prepare input buffer with process ID
+    BYTE inputBuffer[sizeof(HANDLE)] = { 0 };
+    *(PHANDLE)inputBuffer = (HANDLE)processId;
+
+    // Get the required buffer size for module info
+    ULONG moduleInfoSize = 0;
+    if (!DeviceIoControl(hDevice, IOCTL_GET_MODULES, inputBuffer, sizeof(HANDLE), NULL, 0, &bytesReturned, NULL))
+    {
+        if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
+        {
+            printf("Failed to get module info size. Error: %d\n", GetLastError());
+            HeapFree(GetProcessHeap(), 0, pProcessInfo);
+            CloseHandle(hDevice);
+            return 1;
+        }
+
+        moduleInfoSize = bytesReturned;
     }
 
+    // Allocate memory for module info
+    PMODULE_INFO pModuleInfo = (PMODULE_INFO)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, moduleInfoSize);
+    if (pModuleInfo == NULL)
+    {
+        printf("Failed to allocate memory for module info.\n");
+        HeapFree(GetProcessHeap(), 0, pProcessInfo);
+        CloseHandle(hDevice);
+        return 1;
+    }
+
+    // Get module info
+    if (!DeviceIoControl(hDevice, IOCTL_GET_MODULES, inputBuffer, sizeof(HANDLE), pModuleInfo, moduleInfoSize, &bytesReturned, NULL))
+    {
+        printf("Failed to get module info. Error: %d\n", GetLastError());
+        HeapFree(GetProcessHeap(), 0, pModuleInfo);
+        HeapFree(GetProcessHeap(), 0, pProcessInfo);
+        CloseHandle(hDevice);
+        return 1;
+    }
+
+    // Print module info
+    printf("Module Info:\n");
+    for (DWORD i = 0; i < bytesReturned / sizeof(MODULE_INFO); i++)
+    {
+        printf("Base Address: %p, Size: %lu, Name: %ls\n", pModuleInfo[i].Base, pModuleInfo[i].Size, pModuleInfo[i].Name);
+    }
+
+    // Clean up resources
+    HeapFree(GetProcessHeap(), 0, pModuleInfo);
+    HeapFree(GetProcessHeap(), 0, pProcessInfo);
     CloseHandle(hDevice);
+
     return 0;
 }
