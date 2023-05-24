9930cbda7db8e17f93a0235905bba755db81c2dc
ben <blkph0x@gmail.com>
2023-05-25 09:38:21 +1000

    update
    removed the exe and sys

--- User Mode App/UserApp.cpp
+++ User Mode App/UserApp.cpp
@@ -5,7 +5,7 @@
 #define IOCTL_GET_MODULES CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_READ_ACCESS)
 
 #define MAX_PROCESS_INFO_COUNT 8000 // Maximum number of process info entries
-#define MAX_MODULE_INFO_COUNT 4000  // Maximum number of module info entries
+#define MAX_MODULE_INFO_COUNT 8000  // Maximum number of module info entries
 
 typedef struct _PROCESS_INFO {
     HANDLE ProcessId;
@@ -60,12 +60,40 @@ int main()
     // Get process info
     if (!DeviceIoControl(hDevice, IOCTL_GET_PROCESS_IDS, NULL, 0, pProcessInfo, processInfoSize, &bytesReturned, NULL))
     {
-        printf("Failed to get process info. Error: %d\n", GetLastError());
+        if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
+        {
+            printf("Failed to get process info. Error: %d\n", GetLastError());
+            HeapFree(GetProcessHeap(), 0, pProcessInfo);
+            CloseHandle(hDevice);
+            return 1;
+        }
+        printf("Insufficient buffer size for process info. Retry with a larger buffer.\n");
+
+        // Adjust the buffer size and retry the call
+        processInfoSize = bytesReturned;
         HeapFree(GetProcessHeap(), 0, pProcessInfo);
-        CloseHandle(hDevice);
-        return 1;
+        pProcessInfo = (PPROCESS_INFO)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, processInfoSize);
+        if (pProcessInfo == NULL)
+        {
+            printf("Failed to allocate memory for process info.\n");
+            CloseHandle(hDevice);
+            return 1;
+        }
+
+        if (!DeviceIoControl(hDevice, IOCTL_GET_PROCESS_IDS, NULL, 0, pProcessInfo, processInfoSize, &bytesReturned, NULL))
+        {
+            printf("Failed to get process info. Error: %d\n", GetLastError());
+            HeapFree(GetProcessHeap(), 0, pProcessInfo);
+            CloseHandle(hDevice);
+            return 1;
+        }
+
+        printf("Successfully obtained process info after adjusting buffer size.\n");
     }
-    printf("Successfully obtained process info.\n");
+    else
+    {
+        printf("Successfully obtained process info.\n");
+    }
 
     // Print process info
     printf("Process Info:\n");
@@ -75,62 +103,7 @@ int main()
     }
     printf("\n");
 
-    // Prompt user to enter a process ID for module info
-    printf("Enter a process ID to get module info: ");
-    HANDLE processId = 0;
-    scanf_s("%p", &processId);
-
-    // Prepare input buffer with process ID
-    BYTE inputBuffer[sizeof(HANDLE)] = { 0 };
-    *(PHANDLE)inputBuffer = (HANDLE)processId;
-
-    // Get the required buffer size for module info
-    ULONG moduleInfoSize = 0;
-    if (!DeviceIoControl(hDevice, IOCTL_GET_MODULES, inputBuffer, sizeof(HANDLE), NULL, 0, &bytesReturned, NULL))
-    {
-        if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
-        {
-            printf("Failed to get module info size. Error: %d\n", GetLastError());
-            HeapFree(GetProcessHeap(), 0, pProcessInfo);
-            CloseHandle(hDevice);
-            return 1;
-        }
-        printf("Obtained required module info size.\n");
-
-        moduleInfoSize = bytesReturned;
-    }
-
-    // Allocate memory for module info
-    PMODULE_INFO pModuleInfo = (PMODULE_INFO)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, moduleInfoSize);
-    if (pModuleInfo == NULL)
-    {
-        printf("Failed to allocate memory for module info.\n");
-        HeapFree(GetProcessHeap(), 0, pProcessInfo);
-        CloseHandle(hDevice);
-        return 1;
-    }
-    printf("Successfully allocated memory for module info.\n");
-
-    // Get module info
-    if (!DeviceIoControl(hDevice, IOCTL_GET_MODULES, inputBuffer, sizeof(HANDLE), pModuleInfo, moduleInfoSize, &bytesReturned, NULL))
-    {
-        printf("Failed to get module info. Error: %d\n", GetLastError());
-        HeapFree(GetProcessHeap(), 0, pModuleInfo);
-        HeapFree(GetProcessHeap(), 0, pProcessInfo);
-        CloseHandle(hDevice);
-        return 1;
-    }
-    printf("Successfully obtained module info.\n");
-
-    // Print module info
-    printf("Module Info:\n");
-    for (DWORD i = 0; i < bytesReturned / sizeof(MODULE_INFO); i++)
-    {
-        printf("Base Address: %p, Size: %lu, Name: %ls\n", pModuleInfo[i].Base, pModuleInfo[i].Size, pModuleInfo[i].Name);
-    }
-
     // Clean up resources
-    HeapFree(GetProcessHeap(), 0, pModuleInfo);
     HeapFree(GetProcessHeap(), 0, pProcessInfo);
     CloseHandle(hDevice);
     printf("Cleaned up resources and closing the application.\n");
